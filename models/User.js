0d8ee82f671a8245bff0ff6ccd68a94164e3c9ae
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-04-27 13:57:12 +0530

    Fix Mongoose encryption bug for Google Login

--- models/User.js
+++ models/User.js
@@ -33,8 +33,8 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next();
+    if (!this.isModified('password') || !this.password) {
+        return next();
     }
 
     const salt = await bcrypt.genSalt(10);
